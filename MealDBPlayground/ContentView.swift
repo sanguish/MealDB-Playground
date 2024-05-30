9b678497b133eee999729b6fc9735ef6df026da0
Scott Anguish <sanguish@me.com>
2024-05-30 03:28:53 -0400

    Ported from MealDB-DTO

--- MealDBPlayground/ContentView.swift
+++ MealDBPlayground/ContentView.swift
@@ -2,20 +2,50 @@
 //  ContentView.swift
 //  MealDBPlayground
 //
-//  Created by Scott Anguish on 5/30/24.
+//  Created by Scott Anguish on 5/17/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel = ViewModel()
+
+    var recipe: RecipeDetail {
+        viewModel.recipe!
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if viewModel.recipe == nil {
+                ContentUnavailableView {
+                    Label("Oops", systemImage: "exclamationmark.triangle")
+                } description: {
+                    Text("The JSON didn't load.")
+                }
+            } else {
+                if let thumbnail = recipe.mealThumbnailURL {
+                    AsyncImage(url: thumbnail) { image in
+
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .clipShape(.rect(cornerRadius: 16.0))
+                    .clipped()
+                    .shadow(radius: 8)
+                    .frame(width: 400, height: 400)
+                    .padding(10)
+                }
+                Text(recipe.name)
+                    .font(.largeTitle)
+                    .fontDesign(.rounded)
+            }
+
         }
-        .padding()
+        .padding(40)
     }
 }
 
